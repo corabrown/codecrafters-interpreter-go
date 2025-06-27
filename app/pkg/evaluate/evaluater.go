e0b216f75b1579c4a6004bf68af657fd0595b8a3
corabrown <coralynnbrown@gmail.com>
2025-06-27 10:17:26 -0500

    binary evaluation errors

--- app/pkg/evaluate/evaluater.go
+++ app/pkg/evaluate/evaluater.go
@@ -36,62 +36,13 @@ func (i *Interpreter) Evaluate() []*errors.RuntimeError {
 
 func (i *Interpreter) VisitBinary(v data.BinaryExpr) {
 
-	in, exprType := getLeftAndRightVals(v)
+	var valid bool
 
-	if v.Operator.TokenType == data.BANG_EQUAL {
-		i.value = !isEqual(in.left, in.right)
-	}
-	if v.Operator.TokenType == data.EQUAL_EQUAL {
-		i.value = isEqual(in.left, in.right)
-	}
-
-	switch exprType {
-	case floatType:
-		l, r := in.leftFloat, in.rightFloat
-		switch v.Operator.TokenType {
-		case data.MINUS:
-			i.value = l - r
-		case data.SLASH:
-			i.value = l / r
-		case data.STAR:
-			i.value = l * r
-		case data.PLUS:
-			i.value = l + r
-		case data.GREATER:
-			i.value = l > r
-		case data.GREATER_EQUAL:
-			i.value = l >= r
-		case data.LESS:
-			i.value = l < r
-		case data.LESS_EQUAL:
-			i.value = l <= r
+	defer func() {
+		if !valid {
+			i.errors = append(i.errors, errors.NewRuntimeError("Operands must be numbers."))
 		}
-	case stringType:
-		if v.Operator.TokenType == data.PLUS {
-			i.value = in.leftString + in.rightString
-		}
-	}
-}
-
-type binaryInput struct {
-	leftFloat   float64
-	rightFloat  float64
-	leftString  string
-	rightString string
-	left        any
-	right       any
-}
-
-type binaryExpressionType string
-
-const (
-	stringType  binaryExpressionType = "string"
-	floatType   binaryExpressionType = "float"
-	invalidType binaryExpressionType = "invalid"
-)
-
-func getLeftAndRightVals(v data.BinaryExpr) (b binaryInput, exprType binaryExpressionType) {
-	exprType = invalidType
+	}()
 
 	left := NewInterpreter(v.Left)
 	left.Evaluate()
@@ -99,24 +50,78 @@ func getLeftAndRightVals(v data.BinaryExpr) (b binaryInput, exprType binaryExpre
 	right := NewInterpreter(v.Right)
 	right.Evaluate()
 
-	b.left = left.value
-	b.right = right.value
+	l, r := left.value, right.value
 
-	if l, ok := left.value.(float64); ok {
-		if r, ok := right.value.(float64); ok {
-			b.leftFloat = l
-			b.rightFloat = r
-			exprType = floatType
+	switch v.Operator.TokenType {
+	case data.MINUS:
+		if lf, rf, ok := getFloats(l, r); ok {
+			i.value = lf - rf
+			valid = true
 		}
-	} else if l, ok := left.value.(string); ok {
-		if r, ok := right.value.(string); ok {
-			b.leftString = l
-			b.rightString = r
-			exprType = stringType
+	case data.SLASH:
+		if lf, rf, ok := getFloats(l, r); ok {
+			i.value = lf / rf
+			valid = true
 		}
+	case data.STAR:
+		if lf, rf, ok := getFloats(l, r); ok {
+			i.value = lf * rf
+			valid = true
+		}
+	case data.PLUS:
+		if lf, rf, ok := getFloats(l, r); ok {
+			i.value = lf + rf
+			valid = true
+		} else if ls, rs, ok := getStrings(l, r); ok {
+			i.value = ls + rs 
+			valid = true
+		}
+	case data.GREATER:
+		if lf, rf, ok := getFloats(l, r); ok {
+			i.value = lf > rf
+			valid = true
+		}
+	case data.GREATER_EQUAL:
+		if lf, rf, ok := getFloats(l, r); ok {
+			i.value = lf >= rf
+			valid = true
+		}
+	case data.LESS:
+		if lf, rf, ok := getFloats(l, r); ok {
+			i.value = lf < rf
+			valid = true
+		}
+	case data.LESS_EQUAL:
+		if lf, rf, ok := getFloats(l, r); ok {
+			i.value = lf <= rf
+			valid = true
+		}
+	case data.BANG_EQUAL:
+		i.value = !isEqual(l, r)
+		valid = true 
+	case data.EQUAL_EQUAL:
+		i.value = isEqual(l, r)
+		valid = true 
 	}
 
-	return
+}
+
+func getFloats(left, right any) (float64, float64, bool) {
+	if lf, okLeft := left.(float64); okLeft {
+		if rf, okRight := right.(float64); okRight {
+			return lf, rf, true
+		}
+	}
+	return 0, 0, false
+}
+
+func getStrings(left, right any) (string, string, bool) {
+	if ls, okLeft := left.(string); okLeft {
+		if rs, okRight := right.(string); okRight {
+			return ls, rs, true
+		}
+	}
+	return "", "", false
 }
 
 func (i *Interpreter) VisitGrouping(v data.GroupingExpr) {
